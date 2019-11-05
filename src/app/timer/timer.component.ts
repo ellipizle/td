ee9aa98da0aed23101fd4a5511707a69ec388cef
Isaac Bitrus <ellipizle@gmail.com>
2019-11-05 13:18:51 +0100

    feat(timer):create 30 min timer component with play and stop button

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Observable, timer, NEVER, BehaviorSubject, Subject } from 'rxjs';
+import { map, scan, tap, takeUntil, takeWhile, switchMap } from 'rxjs/operators';
+import { Todo } from '../todo';
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent implements OnInit, OnDestroy {
+  private timer$ = new BehaviorSubject(false);
+  private unsubscribe$: Subject<void> = new Subject<void>();
+  running: boolean = false // running state;
+  timeTotal: number = 1800 // 30min in seconds;
+  interval = 1000 // 1 seconds
+  _todo: Todo
+
+
+  @Input()
+  set todo(todo: Todo) {
+    console.log(todo)
+    this._todo = todo;
+  }
+
+
+  constructor() { }
+  ngOnInit() {
+    this.initTimer()
+  }
+
+  initTimer() {
+    this.timer$.pipe(
+      switchMap((running: boolean) => (
+        running ? timer(0, this.interval) : NEVER
+      )),
+      scan(acc => acc - 1, this._todo.timeRemaining ? this._todo.timeRemaining : this.timeTotal),
+      tap(timeValue => this.computeTiming(timeValue)),
+    ).subscribe(value => {
+     console.log(value)
+    })
+  }
+
+  toggle() {
+    this.running = !this.running
+    this.timer$.next(this.running)
+  }
+
+  computeTiming(timeValue) {
+    this._todo.timeRemaining = timeValue
+    this._todo.timeSpent = this.timeTotal - this._todo.timeRemaining;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+
+
+
+}
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -2,6 +2,8 @@ export class Todo {
   id: number;
   title = '';
   complete = false;
+  timeRemaining: number;
+  timeSpent: number;
 
   constructor(values: Object = {}) {
     Object.assign(this, values);
